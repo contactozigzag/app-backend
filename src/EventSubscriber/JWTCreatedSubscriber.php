50cd46441d7d298f1569f5e3da6dad791008ffdf
Piero Recchia <piero.recchia@gmail.com>
2026-01-26 19:17:42 -0300

    Added event subscriber for JWT to add user ID to the JWT payload

---
+++ src/EventSubscriber/JWTCreatedSubscriber.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Events;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class JWTCreatedSubscriber implements EventSubscriberInterface
+{
+    public function onJWTCreatedEvent(JWTCreatedEvent $event): void
+    {
+        $payload = $event->getData();
+        $user = $event->getUser();
+
+        // Add user ID to the JWT payload
+        if (method_exists($user, 'getId')) {
+            $payload['id'] = $user->getId();
+        }
+
+        $event->setData($payload);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            Events::JWT_CREATED => 'onJWTCreatedEvent',
+        ];
+    }
+}
