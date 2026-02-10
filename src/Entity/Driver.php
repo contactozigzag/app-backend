5d7e741d5bee6e91275fc6ba50fad154e82bab75
Piero Recchia <piero.recchia@gmail.com>
2026-02-10 19:26:21 -0300

    Added filter in Driver entity for partial search in nickname

--- src/Entity/Driver.php
+++ src/Entity/Driver.php
@@ -2,13 +2,36 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\PartialSearchFilter;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\QueryParameter;
 use App\Repository\DriverRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: DriverRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new Get(security: "is_granted('ROLE_USER')"),
+        new GetCollection(
+            security: "is_granted('ROLE_USER')",
+            parameters: [
+                'search[:property]' => new QueryParameter(
+                    filter: new PartialSearchFilter(),
+                    properties: ['nickname']
+                )
+            ]
+        ),
+        new Post(security: "is_granted('ROLE_USER')"),
+        new Patch(security: "is_granted('ROLE_DRIVER')"),
+        new Delete(security: "is_granted('ROLE_ADMIN')"),
+    ]
+)]
 class Driver
 {
     #[ORM\Id]
